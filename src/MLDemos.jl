2c0db09de48db3905b2783d4fd5694f92271d3a8
Abhijith C <abhijithc@gmail.com>
2016-06-03 12:47:42 +0000

    remove higgs

--- src/MLDemos.jl
+++ src/MLDemos.jl
@@ -4,7 +4,7 @@ using LightXML, DataFrames, URIParser, Requests
 import Requests: get, options
 include("$(Pkg.dir())/MLDemos/src/drug/types.jl")
 include("$(Pkg.dir())/MLDemos/src/drug/dbquery.jl")
-include("$(Pkg.dir())/MLDemos/src/xgboost/higgs.jl")
+#include("$(Pkg.dir())/MLDemos/src/xgboost/higgs.jl")
 include("$(Pkg.dir())/MLDemos/src/xgboost/mushroom.jl")
 
 # functions
